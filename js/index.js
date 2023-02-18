bce7a2ca51f1344354a4699640f7712693008516
MitakaGated <dimitarpetrov44@gmail.com>
2023-02-18 15:44:50 +0200

    Implemented random number function and choices

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function getComputerChoice() {
+    choices = [Rock, Paper, Scissors];
+}
+randomNumber(0,2);
