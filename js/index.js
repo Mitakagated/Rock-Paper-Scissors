a655231fc163a9b05d05b97abfb12dd64d8a84a0
MitakaGated <dimitarpetrov44@gmail.com>
2023-02-18 15:53:01 +0200

    Implementing the browser choice

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function randomNumber (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 function getComputerChoice() {
-    choices = [Rock, Paper, Scissors];
+    choices = ["Rock", "Paper", "Scissors"];
+    console.log(choices[randomNumber(0,choices.length)]);
 }
-randomNumber(0,2);
+getComputerChoice();
