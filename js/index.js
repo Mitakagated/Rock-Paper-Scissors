30412e07a906dd500cecbfed072e78e9aaccc935
Mitakagated <dimitarpetrov44@gmail.com>
2025-01-08 17:57:28 +0200

    Add UI, remove prompting

--- js/index.js
+++ js/index.js
@@ -4,76 +4,98 @@ let playerSelectionLower = playerSelection.toLowerCase();
 let computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
+let lineCount = 0;
+const buttons = document.querySelectorAll("#choice");
+const result = document.querySelector("#results");
+const playerScoreElement = document.querySelector("#playerScore");
+const computerScoreElement = document.querySelector("#computerScore");
+const playAgainButton = document.querySelector("#playAgain");
 
-function newChoices() {
-    playerSelection = prompt("Rock, Paper or Scissors?");
-    playerSelectionLower = playerSelection.toLowerCase();
-    computerSelection = getComputerChoice();
-}
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerSelection = button.textContent;
+        playerSelectionLower = playerSelection.toLowerCase();
+        computerSelection = getComputerChoice();
+        playRound(playerSelectionLower, computerSelection);
+        playerScoreElement.textContent = playerScore;
+        computerScoreElement.textContent = computerScore;
+    });
+});
+playAgainButton.addEventListener("click", () => {
+    playAgain();
+})
 function randomNumber (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 function getComputerChoice() {
-    choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["Rock", "Paper", "Scissors"];
     return choices[randomNumber(0,choices.length)];
 }
-function playRound (playerSelection, computerSelection) {
-    console.log("Your choice is: " + playerSelection);
-    console.log("Browser's choice is: " + computerSelection);
-    if (playerSelection == "rock" && computerSelection == "Paper") {
-        console.log("You Lose! Paper beats Rock!");
-        return computerScore += 1;
-    } else if (playerSelection == "rock" && computerSelection == "Rock"){
-        console.log("Draw! Rock can't beat Rock!");
-    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
-        console.log("You win! Rock beats Scissors!");
-        return playerScore += 1;
-    } else if (playerSelection == "paper" && computerSelection == "Rock") {
-        console.log("You Win! Paper beats Rock!");
-        return playerScore += 1;
-    } else if (playerSelection == "paper" && computerSelection == "Paper") {
-        console.log("Draw! Paper can't beat Paper!");
-    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
-        console.log("You lose! Scissors beat paper!");
-        return computerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
-        console.log("You lose! Rock beats Scissors!");
-        return computerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
-        console.log("You Win! Scissors beat Paper!");
-        return playerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
-        console.log("Draw! Scissors can't beat Scissors!");
-    } else {
-        let playerSelection = prompt("Please input Rock, Paper or Scissors.");
-        let playerSelectionLower = playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelectionLower, computerSelection);
+function insertText(text, element) {
+    const paragraph = document.createElement("p");
+    paragraph.appendChild(document.createTextNode(text));
+    element.appendChild(paragraph);
+    lineCount += 1;
+    if (lineCount == 3) {
+        const lineBreak = document.createElement("br");
+        element.appendChild(lineBreak);
+        lineCount = 0;
     }
 }
-function game(gameNumber = 5) {
-    for (let i = 0; i < gameNumber; i++) {
-        newChoices();
-        playRound(playerSelectionLower, computerSelection);
+function playRound (playerSelection, computerSelection) {
+    insertText("Your choice is: " + playerSelection, result);
+    insertText("Browser's choice is: " + computerSelection, result);
+    if (playerSelection == "rock" && computerSelection == "Paper") {
+        insertText("You Lose! Paper beats Rock!", result);
+        computerScore += 1;
+    } else if (playerSelection == "rock" && computerSelection == "Rock"){
+        insertText("Draw! Rock can't beat Rock!", result);
+    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+        insertText("You Win! Rock beats Scissors!", result);
+        playerScore += 1;
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        insertText("You Win! Paper beats Rock!", result);
+        playerScore += 1;
+    } else if (playerSelection == "paper" && computerSelection == "Paper") {
+        insertText("Draw! Paper can't beat Paper!", result);
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        insertText("You Lose! Scissors beat Paper!", result);
+        computerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        insertText("You Lose! Rock beats Scissors!", result);
+        computerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        insertText("You Win! Scissors beat Paper!", result);
+        playerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+        insertText("Draw! Scissors can't beat Scissors!", result);
     }
-    playAgain();
+    if (playerScore >= 5 || computerScore >= 5) {
+        getResults();
+        playAgain();
+    }
 }
 function callPlayAgain() {
     playAgain();
 }
 function playAgain() {
     let playAgain = prompt("Do you want to play more? (Yes or No)");
+    if (playAgain == null) {
+        callPlayAgain();
+        return;
+    }
     let playAgainLower = playAgain.toLowerCase();
     if (playAgainLower == "yes") {
-        let gameNumber = prompt("How many rounds would you like to play?");
-        if (gameNumber > 0) {
-            game(gameNumber);
-        } else if (gameNumber <= 0) {
-            return alert("Then we are done!");
-        } else {
-            callPlayAgain();
-        }
+        playerScore = 0;
+        computerScore = 0;
+        result.textContent = "";
+        buttons.forEach((button) => {
+            button.disabled = false;
+        })
     } else if (playAgainLower == "no") {
+        buttons.forEach((button) => {
+            button.disabled = true;
+        })
         getResults();
     } else {
         callPlayAgain();
@@ -81,11 +103,10 @@ function playAgain() {
 }
 function getResults() {
     if (playerScore > computerScore) {
-        return console.log("You beat the browser!");
+        alert("You beat the browser!");
     } else if (computerScore > playerScore) {
-        return console.log("The computer beat you!");
+        alert("The computer beat you!");
     } else if (playerScore === computerScore) {
-        return console.log("The game ended in a draw!");
+        alert("The game ended in a draw!");
     }
 }
-game();
